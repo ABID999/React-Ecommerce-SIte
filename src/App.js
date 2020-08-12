4ba9ce39cf0c71db0da0b54f4be5bee80b642973
ABID999 <abidislam49@gmail.com>
2020-08-13 03:55:50 +0600

    Added gomepage components and basic router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
-import Homepage from './Homepage.component'
+import Homepage from './components/pages/Homepage.component'
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () =>(
+  <h1>This is the Hats page</h1>
+)
+
 function App() {
   return (
     <div >
-      <Homepage />
+      {/* <Homepage /> */}
+      {/* Only render the first matched path */}
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
