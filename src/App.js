f4e886e0a8399c602c31eaf33b738afdc6dc7091
ABID999 <abidislam49@gmail.com>
2020-08-13 14:09:42 +0600

    header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import Homepage from './components/pages/Homepage.component';
 import ShopPage from './components/pages/shop/shop.component'
+import Header from './components/header/header.component';
 
-const HatsPage = () =>(
-  <h1>This is the Hats page</h1>
-)
+
 
 function App() {
   return (
     <div >
       {/* <Homepage /> */}
       {/* Only render the first matched path */}
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
